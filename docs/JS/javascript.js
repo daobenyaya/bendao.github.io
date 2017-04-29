ef46ec2eb7c263fb059759904059d7a24ed03add
bendao <daobenyaya@gmail.com>
2017-04-29 22:50:49 +0200

    c

--- docs/JS/javascript.js
+++ docs/JS/javascript.js
@@ -1,11 +1,41 @@
+function RequeteAPI(url,elementID) {
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
-};
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
-xmlhttp.send();
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var myObj = this.responseText;
+          var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+          document.getElementById(elementID).innerHTML = jsonPretty;
+      }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+
+}
+
+function HomePageLoading1()
+{
+  url = 'http://bitcoin.mubiz.com/info';
+  elementID = 'info';
+  RequeteAPI(url,elementID);
+}
+function HomePageLoading2()
+{
+  url = 'http://bitcoin.mubiz.com/blockchaininfo';
+  elementID = "blockchaininfo";
+  RequeteAPI(url,elementID);
+}
+
+function HomePageLoading3()
+{
+  url = 'http://bitcoin.mubiz.com/mininginfo';
+  elementID = "mininginfo";
+  RequeteAPI(url,elementID);
+}
+
+function HomePageLoading4()
+{
+  url = 'http://bitcoin.mubiz.com/peerinfo';
+  elementID = "peerinfo";
+  RequeteAPI(url,elementID);
+}
